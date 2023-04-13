534fa840f8da537d533948a3b477c6ceb04518bd
ispan <ispan@DESKTOP-PAKSETB>
2023-04-13 13:01:24 +0800

    加入專案檔案。

---
+++ 0413/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _0413
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Member men = new Member("Text");
+			//men.Name = "Test";
+			Console.WriteLine(Member.Greet()+men.Name);
+			var men1 = new demo.imageHelper(); //找到demo資料夾內的imageHelper	
+		}
+	}
+
+
+	public class Member
+	{
+		public string Name { get; set; }
+		//public Member() { }
+		public Member(string name) 
+		{
+			Name = name;
+		}
+
+		public static string Greet()
+		{
+			return "安安";
+		}
+	}
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var estore = new Estore();
+			estore.DoIt(15, 13);
+			//estore.DoIt(15, 13,,88);跳過C這樣不行
+			estore.DoIt(b:99,a:9,d:79); //這樣可以跳過C，還可以換順序
+			estore.DoIt(a: 3,
+						b: 4,
+						d: 78);
+		}
+	}
+
+	public class Estore
+	{
+		public void DoIt(int a, int b,int c=0,int d =-1 ) { }
+		
+
+
+	}
+}
